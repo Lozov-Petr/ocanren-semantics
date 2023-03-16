04f04f6f2209b55fa68448c28def92e03fb3049a
Peter Lozov <not_kill@gmail.ru>
2023-03-17 01:55:11 +0300

    Polish examples.

--- src/examples.ml
+++ src/examples.ml
@@ -1,26 +1,33 @@
 open Substitution
 open Goal
 
+let invoke_appendo a b ab = invoke "appendo" [ a; b; ab ]
+
 let appendo : relation =
   ( "appendo",
     ( [ "a"; "b"; "ab" ],
-      Var "a" === nil &&& (Var "b" === Var "ab")
+      let a, b, ab = (Var "a", Var "b", Var "ab") in
+      a === nil &&& (b === ab)
       ||| fresh [ "h"; "t"; "ab'" ]
-            (Var "a" === cons (Var "h") (Var "t")
-            &&& (cons (Var "h") (Var "ab'") === Var "ab")
-            &&& invoke "appendo" [ Var "t"; Var "b"; Var "ab'" ]) ) )
+            (let h, t, ab' = (Var "h", Var "t", Var "ab'") in
+             a === cons h t &&& (cons h ab' === ab) &&& invoke_appendo t b ab')
+    ) )
+
+let invoke_reverso a b = invoke "reverso" [ a; b ]
+
+let reverso : relation =
+  ( "reverso",
+    ( [ "a"; "b" ],
+      let a, b = (Var "a", Var "b") in
+      a === nil &&& (b === nil)
+      ||| fresh [ "h"; "t"; "a'" ]
+            (let h, t, a' = (Var "h", Var "t", Var "a'") in
+             a === cons h t
+             &&& invoke_appendo a' (cons h nil) b
+             &&& invoke_reverso t a') ) )
 
 let list1 = list constant [ "1" ]
 let list12 = list constant [ "1"; "2" ]
 let list3 = list constant [ "3" ]
 let list123 = list constant [ "1"; "2"; "3" ]
 let list1234 = list constant [ "1"; "2"; "3"; "4" ]
-
-let reverso : relation =
-  ( "reverso",
-    ( [ "a"; "b" ],
-      Var "a" === nil &&& (Var "b" === nil)
-      ||| fresh [ "h"; "t"; "a'" ]
-            (Var "a" === cons (Var "h") (Var "t")
-            &&& invoke "appendo" [ Var "a'"; cons (Var "h") nil; Var "b" ]
-            &&& invoke "reverso" [ Var "t"; Var "a'" ]) ) )
--- src/v2/unify_test.ml
+++ src/v2/unify_test.ml
@@ -1,3 +1,4 @@
+open Substitution
 open Eval
 open Goal
 open Examples
@@ -7,32 +8,57 @@ let __ _ =
   ignore
   @@ (Eval_trace.run ~global_variable:true ~need_log:true)
        [ appendo ] (-1)
-       (fresh [ "q" ] @@ invoke "appendo" [ list12; list3; Var "q" ])
+       (fresh [ "q" ]
+       @@
+       let q = Var "q" in
+       invoke_appendo list12 list3 q)
 
 let __ _ =
   Format.printf "reverso q [1; 2]\n";
   ignore
   @@ (Eval_trace.run ~global_variable:true ~need_log:true)
        [ appendo; reverso ] (-1)
-       (fresh [ "q" ] @@ invoke "reverso" [ Var "q"; list12 ])
+       (fresh [ "q" ]
+       @@
+       let q = Var "q" in
+       invoke_reverso q list12)
 
 let __ _ =
   Format.printf "appendo p q [1; 2; 3; 4] q\n";
   ignore
   @@ (Eval_trace.run ~global_variable:true ~need_log:true)
        [ appendo ] (-1)
-       (fresh [ "q"; "p" ] @@ invoke "appendo" [ Var "q"; Var "p"; list1234 ])
+       (fresh [ "q"; "p" ]
+       @@
+       let q, p = (Var "q", Var "p") in
+       invoke_appendo q p list1234)
 
 let __ _ =
   Format.printf "reverso q [1]\n";
   ignore
   @@ (Eval_trace.run ~global_variable:true ~need_log:true)
        [ appendo; reverso ] (-1)
-       (fresh [ "q" ] @@ invoke "reverso" [ Var "q"; list1 ])
+       (fresh [ "q" ]
+       @@
+       let q = Var "q" in
+       invoke_reverso q list1)
 
-let _ =
+let __ _ =
   Format.printf "reverso q [1; 2]\n";
   ignore
   @@ (Eval_trace.run ~global_variable:true ~need_log:true)
        [ appendo; reverso ] (-1)
-       (fresh [ "q" ] @@ invoke "reverso" [ Var "q"; list12 ])
+       (fresh [ "q" ]
+       @@
+       let q = Var "q" in
+       invoke_reverso q list12)
+
+let _ =
+  Format.printf "reverso [1; 2] q\n";
+  ignore
+  @@ (Eval_trace.run ~global_variable:true ~need_log:true)
+       [ appendo; reverso ] 21
+       (fresh [ "q" ]
+       @@
+       let q = Var "q" in
+       invoke_reverso list12 q)
